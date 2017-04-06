46f7b2feccdd6bd7e77f9992851e0ced7b054e51
panosgt7 <panos_gt7@hotmail.com>
2017-04-07 01:05:25 +0300

    modified App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,60 @@
+
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header page-scroll">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+          </button>
+          <a class="navbar-brand" href="#">"CHUCK NORRIS"</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav navbar-right">
+            <li class="page-scroll">
+              <router-link to="/home">Home</router-link>
+            </li>
+            <li class="page-scroll">
+              <router-link to="/login" v-show="!user.authenticated">Login</router-link>
+            </li>
+            <li class="page-scroll">
+              <router-link to="/signup" v-show="!user.authenticated">Sign Up</router-link>
+            </li>
+            <li class="page-scroll">
+              <router-link to="/secretquote" v-show="user.authenticated">Secret Quote</router-link>
+            </li>
+            <li class="page-scroll">
+              <a href="#" v-show="user.authenticated" @click="logout()" >Logout</a>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+
+    </nav>
+
+    <div class="container">
+      <router-view></router-view>
+    </div>
+
   </div>
+
 </template>
 
 <script>
-import Hello from './components/Hello'
-
-export default {
-  name: 'app',
-  components: {
-    Hello
-  }
-}
+  import('./assets/js/bootstrap.min.js')
+  import('./assets/js/jquery.min.js')
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "./assets/css/bootstrap.css";
+  @import "./assets/css/style.css";
+  @import "./assets/css/font-awesome.min.css";
+  @import "https://fonts.googleapis.com/css?family=Montserrat:400,700";
+  @import "https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic";
+
 </style>
