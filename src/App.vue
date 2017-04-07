1bc86c79d1506f21bd9e0305ce702dff39f439c0
panosgt7 <panos_gt7@hotmail.com>
2017-04-07 19:02:18 +0300

    make use of minified css

--- src/App.vue
+++ src/App.vue
@@ -51,7 +51,7 @@
 </script>
 
 <style>
-  @import "./assets/css/bootstrap.css";
+  @import "./assets/css/bootstrap.min.css";
   @import "./assets/css/style.css";
   @import "./assets/css/font-awesome.min.css";
   @import "https://fonts.googleapis.com/css?family=Montserrat:400,700";
