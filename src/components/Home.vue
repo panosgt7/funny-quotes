6bfe6d5e3716395f05e0e7eea43022ef4154b507
panosgt7 <panos_gt7@hotmail.com>
2017-04-08 00:29:46 +0300

    added Home component

---
+++ src/components/Home.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="col-sm-6 col-sm-offset-3">
+      <img class="img-responsive" src="../assets/img/profile1.png" alt="">
+      <h1>Get a Free Chuck Norris Quote!</h1>
+      <button class="btn btn-primary" v-on:click="getQuote()">Get a Quote</button>
+      <div class="quote-area" v-if="quote">
+        <h2><blockquote>{{ quote }}</blockquote></h2>
+      </div>
+    </div>
+  </template>
+
+  <script>
+  export default {
+    data() {
+      return {
+        quote: ''
+      }
+    },
+    methods: {
+      getQuote() {
+        this.$http.get('').then(response => {
+          this.quote = response.data;
+        }, response => {
+          console.log(err)
+        });
+      }
+    }
+  }
+  </script>
+
+  <style>
+  .quote-area h2 {
+    color: black;
+  }
+  </style>
