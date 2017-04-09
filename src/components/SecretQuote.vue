6b50a6eff5fdaf298dafdb47026e7379ca10d648
panosgt7 <panos_gt7@hotmail.com>
2017-04-09 15:41:06 +0300

    added seqretQuote component

---
+++ src/components/SecretQuote.vue
@@ -0,0 +1,29 @@
+<template>
+   <div class="col-sm-6 col-sm-offset-3">
+     <img class="img-responsive" src="../assets/img/profile2.png" alt="">
+     <h1>Get a Secret Chuck Norris Quote!</h1>
+     <button class="btn btn-warning" v-on:click="getQuote()">Get a Quote</button>
+     <div class="quote-area" v-if="quote">
+       <h2><blockquote>{{ quote }}</blockquote></h2>
+     </div>
+   </div>
+ </template>
+
+ <script>
+ export default {
+   data() {
+     return {
+       quote: ''
+     }
+   },
+   methods: {
+     getQuote() {
+       this.$http.get('').then(response => {
+           this.quote = response.data;
+       }, response => {
+        console.log(err)
+      });
+     }
+   }
+ }
+ </script>
