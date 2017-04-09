0b335a3b8997d81747d746804e430cc66934b4fa
panosgt7 <panos_gt7@hotmail.com>
2017-04-09 16:04:48 +0300

    create Signup.vue

---
+++ src/components/Signup.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="col-sm-4 col-sm-offset-4">
+      <h2>Sign up</h2>
+      <p>Sign up to get some great secret quotes. You must not miss them!</p>
+      <div class="alert alert-danger" v-if="error">
+        <p>{{ error }}</p>
+      </div>
+      <div class="form-group">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Enter your username"
+          v-model="credentials.username"
+        >
+      </div>
+      <div class="form-group">
+        <input
+          type="password"
+          class="form-control"
+          placeholder="Enter your password"
+          v-model="credentials.password"
+        >
+      </div>
+      <button class="btn btn-primary" @click="submit()">Access</button>
+    </div>
+  </template>
+
+  <script>
+  </script>
